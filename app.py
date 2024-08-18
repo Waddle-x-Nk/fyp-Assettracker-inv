dd27625bb5aa4f90961840409965514b76399715
Kay09xx <100805243+Kay09xx@users.noreply.github.com>
2024-08-18 12:03:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, redirect, jsonify, Response, 
 import sqlite3
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///OIS.db'
 app.secret_key = 'your_secret_key_here'
